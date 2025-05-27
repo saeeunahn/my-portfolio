0da0119c990530df847920698af1455085ee63e6
saeeunahn <115342203+saeeunahn@users.noreply.github.com>
2025-05-27 19:05:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// 항상 0부터 시작하도록 설정
-let coin = 0;
+// 세션 스토리지에서 coin 가져오기 (없으면 0으로 시작)
+let coin = sessionStorage.getItem("coin") || 0;
+coin = parseInt(coin);
 updateCoinDisplay();
 
+// 버튼 클릭 시 재화 증가
 function gainCoins() {
   coin += 50;
+  sessionStorage.setItem("coin", coin);  // 세션에 저장
   updateCoinDisplay();
 }
 
+// 화면에 코인 표시
 function updateCoinDisplay() {
   const el = document.getElementById("coin-count");
   if (el) el.innerText = coin;
 }
-
